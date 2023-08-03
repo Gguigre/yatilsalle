44bc32c12df70f5cb668315ce10c0efab1d63ab4
Guillaume Gremillet <guillaumegremillet@MacBook-Pro-de-Guillaume.local>
2023-08-03 17:35:39 +0200

    feat: add google auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.google) {
+        window.google.accounts.id.initialize({
+          client_id: '627921590766-95uqkbkc7ic29fcrbtc9nb61f8bp3i3g.apps.googleusercontent.com',
+          callback: handleLogin,
+        });
+        window.google.accounts.id.renderButton(
+          document.getElementById('buttonDiv'),
+          { theme: 'outline', size: 'large' }  // options
+        );
+        clearInterval(interval);
+      }
+    }, 100);
+  }, []);
+
+  const handleLogin = (response) => {
+    if (response.credential) {
+      console.log(response.credential)
+      // L'utilisateur s'est connecté avec succès
+      // Tu peux utiliser response.credential pour faire des requêtes à l'API Google Calendar
+    } else {
+      console.warn('Erreur lors de la connexion');
+      // L'utilisateur n'a pas réussi à se connecter
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="buttonDiv"></div>
     </div>
   );
 }
