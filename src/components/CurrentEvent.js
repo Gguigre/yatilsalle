1f5a2ddc653f3d06777964420310aeea9067cb8f
Guillaume Gremillet <gui.gremi@gmail.com>
2023-08-04 11:32:17 +0200

    feat: add current room under current event

--- src/components/CurrentEvent.js
+++ src/components/CurrentEvent.js
@@ -1,11 +1,16 @@
 export const CurrentEvent = ({ currentEvent }) => {
-  console.log(currentEvent);
+  
+  const room = currentEvent.attendees.filter((attendee) => attendee.resource)[0];
+
   return (
     <div className="current-event">
       <h2>Événement en cours : {JSON.stringify(currentEvent.summary)}</h2>
       <p>
-        {/* De {new Date(currentEvent.start.dateTime).toLocaleTimeString()} à{" "}
-        {new Date(currentEvent.end.dateTime).toLocaleTimeString()} */}
+        De {new Date(currentEvent.start.dateTime).toLocaleTimeString()} à{" "}
+        {new Date(currentEvent.end.dateTime).toLocaleTimeString()}
+      </p>
+      <p>
+      {room && ` ${room.displayName}`}
       </p>
     </div>
   );
